b25d1f5f344ac5170a60971b1b5fcd02501a7486
leprekorn <leprekorn@yandex.ru>
2026-01-23 07:05:35 +0500

    chapter05 Added cleanup queries

--- Queries.sql
+++ Queries.sql
@@ -4,3 +4,8 @@ select * from batches;
 select * from allocations;
 select * from order_lines;
 
+
+-- Manual cleaning
+truncate table allocations;
+truncate table batches CASCADE;
+truncate table order_lines CASCADE;
